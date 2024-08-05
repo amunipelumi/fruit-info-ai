1ec29e6bd8cb3be0d05f8e7ba470bd424e3426f8
amunipelumi <amunipelumi@gmail.com>
2024-08-05 14:34:41 +0100

    For Ubuntu

--- fruit-info.sh
+++ fruit-info.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# activate environment
+source /home/amuni/my_applications/fruit-info-ai/venv/bin/activate
+# collect static
 python3 manage.py collectstatic --noinput
+# migrate just in case
 python3 manage.py migrate 
+# run gunicorn command
 gunicorn -w 4 --worker-class=gevent -b 127.0.0.1:5000 fruit_info.wsgi:application
