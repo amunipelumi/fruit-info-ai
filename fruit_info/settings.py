a5d81ec8c519252ce12807c113ed75fb3352e7ca
amunipelumi <amunipelumi@gmail.com>
2024-08-27 10:40:16 +0100

    For Ubuntu

--- fruit_info/settings.py
+++ fruit_info/settings.py
@@ -234,7 +234,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 
-EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
 
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS').lower() in ('true')
 
